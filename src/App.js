a6592b4f7a72810e662793a91415d713633c962b
Stefana <farcasus@yahoo.com>
2021-12-13 10:49:04 +0200

    collection-item

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homePage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/shop" element={<ShopPage />} />
         </Routes>
       </Router>
     </div>
