5b0037626110a502f0d1f24e16aea8954aec2031
Stefana <farcasus@yahoo.com>
2022-01-07 17:56:06 +0200

    removeing or ading itels in checkout

--- src/redux/cart/cart.utils.jsx
+++ src/redux/cart/cart.utils.jsx
@@ -11,3 +11,27 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+// export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+//   const existingCartItem = cartItems.find(
+//     (cartItem) => cartItem.id === cartItemToRemove.id
+//   );
+
+//   if (existingCartItem.quantity === 1) {
+//     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+//   }
+
+//   return cartItems.map((cartItem) =>
+//     cartItem.id === cartItemToRemove.id
+//       ? { ...cartItem, quantity: cartItem.quantity - 1 }
+//       : cartItem
+//   );
+// };
+
+export const removeItemFromCart = (cartItems, itemToRemove) =>
+  cartItems.flatMap((item) => {
+    return item.id !== itemToRemove.id
+      ? item
+      : item.quantity === 1
+      ? []
+      : { ...item, quantity: item.quantity - 1 };
+  });
